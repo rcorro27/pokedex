7b4f725c38bdff280dc5be9d9a0cdc08137d2726
Richard C <richardcorro92@gmail.com>
2020-09-29 23:12:28 -0400

    dossiers ajouter, index creer, main page cree

---
+++ client/src/app/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import {MainPage} from '../pages'
+function App() {
+  return (
+    <MainPage/>
+  );
+}
+
+export default App;
---
+++ client/src/pages/MainPage.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div.attrs({
+    className: 'form-group',
+})`
+    margin: 0 30px;
+`
+const Title = styled.h1.attrs({
+    className: 'h1',
+})``
+
+const Subtitle = styled.h2
+`
+background-color : black;
+color:white;
+`
+class MainPage extends Component{
+   
+
+render(){
+    return(
+        <Wrapper>
+            <Title>Welcome to Pokedex</Title>
+             <Subtitle>hello teste de styled syntaxe assez particulier </Subtitle>
+
+        </Wrapper>
+
+        
+
+
+    )
+
+
+}
+
+
+}
+export default MainPage
