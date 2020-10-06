1b9c0a1c9a650a9b72a0ac6b4d8e0d53ab9f89f5
Richard C <richardcorro92@gmail.com>
2020-10-06 17:09:38 -0400

    ajoute de tables a finir

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -58,23 +58,67 @@ render(){
     
  
     return(
-        <Wrapper className=".container">
-            <Title>Welcome Young Adventured</Title>      
+        <Wrapper className=".container border border-danger rounded bg-danger" >
+            <Title>Welcome Young Adventured</Title>    
+            <h3 className="bg-warning w-50 border border-warning rounded m-1">Name :{this.state.pokemon.name}</h3>  
             {/*<Wrapper className="list-group w-50">
                 {(this.state.pokemon ==='') ? '' : this.state.pokemon.results.map((info,index) => {      
                 return <A className="list-group-item list-group-item-action w-25 rounded shadow-sm" href={info.url} key={index} >{info.name} </A>
                 })}
             </Wrapper>*/}
             <div>
-                 <Title>Name :{this.state.pokemon.name}</Title>
-                 <Title>Order :{this.state.pokemon.order}</Title>
-                 
-                 <Title>Stats : { this.state.pokemon.stats === undefined ? <p>No data</p> : this.state.pokemon.stats[0].stat.name}</Title>
-                 <Title>Type : {this.state.pokemon.types === undefined ? <p>No Data</p> : this.state.pokemon.types[0].type.name}</Title>
-        
+                
                  <Img className="img-thumbnail" src={this.state.pokemon.sprites === undefined ? "undefined" : this.state.pokemon.sprites.front_default} alt={this.state.pokemon.name}/>
+                 <Title>Order :{this.state.pokemon.order}</Title>
+                 <div className="bg-light w-25">
+<h3>Stats :</h3>
 
+                 <table className="border border-dark rounded">
+                     <thead>
+                      <tr>
+                        <th>Name</th>
+                         <th>Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        { this.state.pokemon.stats === undefined ? <td>No data</td> : this.state.pokemon.stats.map((info, index) => { 
+                           return(
+                            
+                           <td >{info.stat.name}</td>
+                           
+                          
+                            )
+                         })}
+                         </tr>
+                    </tbody>
+                        
+                    
+                    <tfoot>
+                    </tfoot>
+                    </table>
+                 
+                
+                  </div>
             </div>
+                 <Title>Type : {this.state.pokemon.types === undefined ? <p>No Data</p> : this.state.pokemon.types[0].type.name}</Title>
+                <div>
+                    <h2>Abilities :</h2>
+                    {this.state.pokemon.abilities === undefined ? <p>No Data</p> : this.state.pokemon.abilities.map((info ,index) => {      
+                        return <A className="list-group-item list-group-item-action w-25 rounded shadow-sm" href={info.ability.url} key={index} >{info.ability.name} </A>
+                    })}
+                </div>    
+                <div>
+                    <h1>Moves :</h1>
+                    {this.state.pokemon.moves === undefined ? <p>No Data</p> : this.state.pokemon.moves.map((info, index)=> {
+                        return <A className="list-group-item list-group-item-action w-25 rounded shadow-sm" href={info.move.url} key={index} >{info.move.name} </A>
+
+                    })}    
+                </div>            
+        
+                 
+
+            
         </Wrapper>
     )
 }
