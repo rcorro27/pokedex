76529f2d830ba0028dcd0ee1e776aaef5af6686b
Richard C <richardcorro92@gmail.com>
2020-10-07 18:27:41 -0400

    import de chart dans le index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,8 +2,9 @@ import Navbar from './Navbar'
 import Logo from './Logo'
 import Titres from './Titres'
 import Search from './Search'
+import Chart from './Chart'
 
 
 
 
-export {Navbar,Logo,Titres,Search}
+export {Navbar, Logo, Titres, Search, Chart}
