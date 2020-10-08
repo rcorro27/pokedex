eed5fa52ed5c25fa4037ca070ee9572b155560f4
Richard C <richardcorro92@gmail.com>
2020-10-08 15:33:43 -0400

    detailles a voir dans le chart

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import styled from 'styled-components'
-import { VictoryPie  } from 'victory'
+import { VictoryPie, VictoryLabel, VictoryTheme, VictoryBar, VictoryChart, VictoryPolarAxis } from 'victory'
 
 
 class Chart extends Component{
@@ -8,20 +8,25 @@ class Chart extends Component{
 
 render(){
     //SP = special-Attack
-    //SD = special-Defense
+    //SD = special-Defense 
+    // function a creer avec des paramettres pour creer un tableau avec les stats 
+    // chart a changer des components a containers 
     
     return(
         <VictoryPie
+            height={this.props.height}
+            width={this.props.width}
             colorScale={["red", "yellow", "blue", "orange", "pink", "green" ]}
             data={[
-            { x: 1, y: this.props.data[0].base_stat, label: this.props.data[0].stat.name },
-            { x: 2, y:this.props.data[1].base_stat, label: this.props.data[1].stat.name },
-            { x: 3, y:this.props.data[2].base_stat, label: this.props.data[2].stat.name },
-            { x: 4, y:this.props.data[3].base_stat, label: this.props.data[3].stat.name },
-            { x: 5, y:this.props.data[5].base_stat, label: this.props.data[5].stat.name },
-            { x: 6, y:this.props.data[4].base_stat, label: this.props.data[4].stat.name }
+            { x: 1, y: this.props.data[0].base_stat, label: this.props.data[0].stat.name+" "+this.props.data[0].base_stat },
+            { x: 2, y:this.props.data[1].base_stat, label: this.props.data[1].stat.name+" "+this.props.data[1].base_stat },
+            { x: 3, y:this.props.data[2].base_stat, label: this.props.data[2].stat.name+" "+this.props.data[2].base_stat },
+            { x: 4, y:this.props.data[3].base_stat, label: this.props.data[3].stat.name+" "+this.props.data[3].base_stat },
+            { x: 5, y:this.props.data[4].base_stat, label: this.props.data[4].stat.name+" "+this.props.data[4].base_stat },
+            { x: 6, y:this.props.data[5].base_stat, label: this.props.data[5].stat.name+" "+this.props.data[5].base_stat }
             ]}
         />
+        
     )
 }
 
