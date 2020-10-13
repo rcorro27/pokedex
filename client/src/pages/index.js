a712e98122ebbd0d2cbd7f71c4118fe178799903
Richard C <richardcorro92@gmail.com>
2020-10-13 15:16:05 -0400

    class move ajoutee

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,6 @@
 import MainPage from './MainPage'
 import ErrorPage from './ErrorPage'
 import ErrorBoundary from './ErrorBoundary'
+import Moves from './Moves'
 
-export {MainPage, ErrorPage, ErrorBoundary}
+export {MainPage, ErrorPage, ErrorBoundary, Moves}
