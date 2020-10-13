02e034a95c7f93b5b756b51abfd6438a86e14767
Richard C <richardcorro92@gmail.com>
2020-10-13 15:16:17 -0400

    classe move creer a tester

---
+++ client/src/pages/Moves.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+class Moves extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            moves:"",
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h1>Moves to be inserted here </h1>
+
+            </div>
+        )
+    }
+}
+export default Moves 
